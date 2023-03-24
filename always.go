6254fef318db6d9e655e0d139bd2290c47a05177
wafer-bw <ben.jz.wakeford@gmail.com>
2023-03-24 17:35:18 -0400

    Remove unused code

--- always.go
+++ always.go
@@ -1,11 +1,5 @@
 package always
 
-import "errors"
-
-var (
-	ErrNotOK = errors.New("not ok")
-)
-
 // Must is used by wrapping a call to a constructor style function, it will panic with the error if it is non-nil.
 func Must[T any](t T, err error) T {
 	if err != nil {
