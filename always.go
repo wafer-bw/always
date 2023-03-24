194f27b5508ce280db75596dccccf09f24758693
wafer-bw <ben.jz.wakeford@gmail.com>
2023-03-24 13:06:14 -0400

    Improve wording

--- always.go
+++ always.go
@@ -6,7 +6,7 @@ var (
 	ErrNotOK = errors.New("not ok")
 )
 
-// Must is used to wrap a call to a constructor style function, will panic with the error if it is non-nil
+// Must is used by wrapping a call to a constructor style function, it will panic with the error if it is non-nil.
 func Must[T any](t T, err error) T {
 	if err != nil {
 		panic(err)
